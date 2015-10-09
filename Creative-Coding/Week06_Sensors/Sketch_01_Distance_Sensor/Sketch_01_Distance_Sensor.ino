8bef6ce0b81242b8a3c9c5ca6e4c1b1a09d7b7e4
01010101 <jerome@saint-clair.net>
2015-10-09 13:57:42 +0200

    CC - Week 06

---
+++ Creative-Coding/Week06_Sensors/Sketch_01_Distance_Sensor/Sketch_01_Distance_Sensor.ino
@@ -0,0 +1,31 @@
+/*
+HC-SR04 Ping distance sensor]
+VCC to arduino 5v GND to arduino GND
+Echo to Arduino pin 13 Trig to Arduino pin 12
+*/
+
+#define trigPin 13
+#define echoPin 12
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  //  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+
+  // Distance in cm
+
+
+  delay(500);
+}
