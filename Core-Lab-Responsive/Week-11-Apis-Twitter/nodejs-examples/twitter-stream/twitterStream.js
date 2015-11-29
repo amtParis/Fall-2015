d6fd876f93b71b6dc9687f5f061afd8f8f9caf1f
chris <csugrue@gmail.com>
2015-11-29 17:08:42 +0100

    add finals example code

--- Core-Lab-Responsive/Week-11-Apis-Twitter/nodejs-examples/twitter-stream/twitterStream.js
+++ Core-Lab-Responsive/Week-11-Apis-Twitter/nodejs-examples/twitter-stream/twitterStream.js
@@ -1,10 +1,10 @@
 var Twit = require('twit')
 
 var T = new Twit({
-    consumer_key:         'zb8z7z7cqeQ3Wz1ZdZNUyxJCj'
-  , consumer_secret:      'FcxjfHgjGfOedMjWvIXlwE7SuFQGQ4snVd6NfFWgxKW2ev3hJ1'
-  , access_token:         '166312715-JHFN4Xmv0GYLqmSZTfKmNjryAftSMGfSzeXuFgpZ'
-  , access_token_secret:  'l3kXEmj7cCd4XoqZBsPz9WyYP1hoVB10iG42sAbtbpvnw'
+    consumer_key:         ''
+  , consumer_secret:      ''
+  , access_token:         ''
+  , access_token_secret:  ''
 })
 
 
@@ -25,6 +25,10 @@ stream.on('tweet', function (tweet) {
 	  console.log(tweet.created_at);
   }
 
+  var su = new SpeechSynthesisUtterance();
+  su.lang = "es";
+  su.text = "hola";
+  //speechSynthesis.speak(su);
   // other params:
   // tweet.id - id of tweet
   // tweet.user.screen_name - twitter name
